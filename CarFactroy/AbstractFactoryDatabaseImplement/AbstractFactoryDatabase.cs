dfeba66ac9916dc73730722e23a8437fcf751a58
Мария Ли <0512masha@gmail.com>
2020-06-22 21:54:17 +0400

    7upd

--- CarFactroy/AbstractFactoryDatabaseImplement/AbstractFactoryDatabase.cs
+++ CarFactroy/AbstractFactoryDatabaseImplement/AbstractFactoryDatabase.cs
@@ -10,7 +10,7 @@ namespace AbstractFactoryDatabaseImplement
         {
             if (optionsBuilder.IsConfigured == false)
             {
-                optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-MRMDEGR0\SQLEXPRESS;Initial Catalog=AbstractFactoryDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+                optionsBuilder.UseSqlServer(@"Server=(local)\SQLEXPRESS;Initial Catalog=AbstractFactoryDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
             }
             base.OnConfiguring(optionsBuilder);
         }
